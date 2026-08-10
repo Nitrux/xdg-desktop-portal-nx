4e75600b67d631d970b2052b43b8251a0fe23a91
Uri Herrera <hello@uriherrera.com>
2026-08-10 02:00:29 -0600

    Fix build script

--- scripts/install-build-deps.sh
+++ scripts/install-build-deps.sh
@@ -1,10 +1,47 @@
 #!/usr/bin/env bash
+
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright 2025-2026 <Nitrux Latinoamericana S.C. <hello@nxos.org>>
+
+
+# -- Exit on errors.
+
 set -e
-if [ "$EUID" -ne 0 ]; then APT_COMMAND="sudo apt"; else APT_COMMAND="apt"; fi
+
+
+# -- Check if running as root.
+
+if [ "$EUID" -ne 0 ]; then
+    APT_COMMAND="sudo apt"
+else
+    APT_COMMAND="apt"
+fi
+
+
+# -- Install build packages.
+
 $APT_COMMAND update -q
-$APT_COMMAND install -y --no-install-recommends ca-certificates curl gnupg
+$APT_COMMAND install -y --no-install-recommends \
+    build-essential \
+    cmake \
+    curl \
+    git \
+    libkf6coreaddons-dev \
+    libkf6i18n-dev \
+    liblayershellqtinterface-dev \
+    libwayland-dev \
+    meson \
+    ninja-build \
+    pkg-config \
+    qt6-base-dev \
+    qt6-declarative-dev \
+    qt6-declarative-dev-tools
+
+
+# -- Add package from our repository.
 
 mkdir -p /etc/apt/keyrings
+
 curl -fsSL https://packagecloud.io/nitrux/mauikit/gpgkey | gpg --dearmor -o /etc/apt/keyrings/nitrux-mauikit.gpg
 
 cat <<EOF > /etc/apt/sources.list.d/nitrux-mauikit.sources
@@ -16,5 +53,7 @@ Components: main
 Signed-By: /etc/apt/keyrings/nitrux-mauikit.gpg
 Enabled: yes
 EOF
+
 $APT_COMMAND update -q
-$APT_COMMAND install -y --no-install-recommends build-essential cmake curl dpkg-dev extra-cmake-modules gnupg pkg-config qt6-base-dev qt6-declarative-dev qt6-wayland-dev mauikit mauikit-filebrowsing
+$APT_COMMAND install -y --no-install-recommends \
+    mauikit-filebrowsing
